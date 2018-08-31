680d97a2bc457a1ef1d7766e30a18839b8634e19
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-30 20:17:01 -0700

    Add files via upload

---
+++ CNIT 105 IT Customer Support WK 2.py
@@ -0,0 +1,32 @@
+#==================================================================
+#			            IT Customer Support
+#==================================================================
+
+''' 
+Date    : 
+Subject : CNIT 105 IT Customer Support
+Title   : 
+Source  : 
+''' 
+
+# Chapter 2
+
+'''
+PG 77 
+NOTES:
+
+Homework Activity 2-6
+
+   Evaluate your communication skills.  Take a 25-question quiz on your personal 
+communication skills at www.optimalthinking.com/quiz-communication-skills.asp  Be as honest 
+with your response as you can.  List any of the 25 items for which you checked sometimes or
+rarely.  Then describe any patterns or themes that you can identify in the items you checked as
+sometimes or rarely.  Finally, what resources are available to you to address the need for skill 
+improvements that you identified.  
+
+
+
+
+
+
+'''
