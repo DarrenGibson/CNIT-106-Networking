44b23c94716a77f18ad76d7d247ab0a86b92da75
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-03 12:43:22 -0700

    Add files via upload

---
+++ CNIT106Ch1Notes.py
@@ -0,0 +1,238 @@
+#==================================================================
+#			              Networking
+#==================================================================
+
+''' 
+Date    : Fri 24 Aug 2018 08:40:42 PM PDT 
+Subject : CNIT 106 Networking
+Title   : Computer Network Fundamentals
+Source  : CNIT 106 Text Book - CompTia Network+ Cert N10-007 
+''' 
+
+#
+
+'''
+NOTES:
+
+'''
+
+# Chapter 1 
+
+'''
+Chapter Summary Questions
+
+1.  Which of the folllowing provides the media ( wired or wireless) to facilitate the 
+transfer of data?
+
+a. Server
+b. LAN 
+c. Client
+d. Router
+
+	b. The local arra network (LAN) consists of many different components.
+	One critical aspect is the media (wired or wireless) that can transmit the 
+	data from node to node 
+
+2.  Which device makes traffic-forwarding decisions based of MAC addresses? 
+
+a. Hub
+b. Router
+c. Switch
+d. Multiplexer
+
+	c. Switches forward traffic based on the sedtination MAC (hardware)
+	address contained in the data frame.  Contrast this to a router, which
+	makes forwarding decidiond based on the sedtination IP address 
+
+3. A company has a various locations in a city interconnected using Metro Ether-
+net connections. This is an example of what type of network?
+
+a.
+b.
+c.
+d.
+
+	d. The metropolitan area network is superb for connecting various build-
+	ings in a citywide area. Metro Etherenet is one example of connectivity
+	techology for the design.
+
+4. A network formed by interconnecting a PC to a digital camera via a USB cable 
+is considered what type of network?
+
+a. WAN
+b. CAN
+c. PAN
+d. MAN
+
+	c. The personal area network features fewer nodes than typically found in
+	a LAN. This design also features a dmall geographic area, such as a car or 
+	a single room office. 
+
+5.  Which of the following physical LAN topologies requires the most cabling?
+
+a. Bus 
+b. Ring
+c. Star
+d. WLAN
+
+	c. The star topology is common today in switched LAN's. However, it 
+	does feature quite a bit of cabling, as the switched require media to each
+	and every node (wired) that needs to communicate.  There are also typi-
+	cally trunk connections between the switched for an extended star topol-
+	ogy, which requires even more cabling 
+
+6. Which of the followoing topologies offers the highest level of redundancy
+
+a. Full mesh
+b. Hub and spoke 
+c. Bus 
+d. Partial mesh
+
+	a. The full-mesh topology might be complex and expensive to implement, 
+	but it does provide excellent levels of redundancy. Should one or more 
+	links fail, it is often possible to reroute traffic around the failures.	
+	
+how many wan loinks are required to creat a full mesh of connections 
+between fice remote sites?
+
+a. 5
+b. 10
+c. 15
+d. 20 
+
+	b. Remember, the formula for the number of connections required for the 
+	full-mesh topology is n(n-1)/2, where n is the number of nodes.  So, in this 
+	case, 5(5-1)/2 is 10
+
+8. Indentify two advantages of a hub-and-spoke WAN topology as compared to a 
+full-mesh WAN topology
+
+a. Lower cost
+b. Optimal routes 
+c. More scalable 
+d. More redundancy
+
+	a, c. THE hub and spoke is actually a form of a partial-mesh topology.
+	This topology has the advantage of being less complex, less costly, and 
+	overall more scalable than a full-mesh design.
+
+9. Which type of network is based on network clients sharing resources with one
+another?
+
+a. Client/Server
+b. Client-peer
+c. Peer to peer
+d. Peer to server
+
+	c. The peer to peer (P2P) network features clients sharing information
+	directly with each other.  
+
+10. Which of the following is an advantage of a peer-to-peer network, as comp-
+pared with a client/server network?
+
+a. More scalable 
+b. Less Expansive 
+c. Better Performance
+d. Simplified administration 
+
+	The P2P network is often celebrated due to its simplicity and low cost.
+	Unfortunately, it is not scalble and can actually cause administration night-
+	mares when attempting to scale it.
+
+11. What type of wireless topology features the use of access points to facilitate 
+network communications?
+
+a. Ad Hoc 
+b. Mesh
+c. Infrastructure 
+d. Ring
+
+	c. The infrastructure type of topology features specialized nodes (access 
+	points) that permit commucnications between other nodes.
+
+12. What network type would help facilitate communications when large video or 
+audio files need to be housed and transfered through the network?
+
+a. WLAN
+b. CAN
+c. PAN
+d. SAN
+
+	d. The storage area network (SAN) assists when large amounts of data must be
+	stored in the network and transfered efficiently from various nodes to other 
+	nodes.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
